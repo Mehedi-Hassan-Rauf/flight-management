1d12a079b6f5b2b0033d5cc29832256dda87eb7a
Rauf <raufnx@gmail.com>
2025-01-11 15:03:37 +0600

    removed '

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -91,7 +91,7 @@ export default function Login() {
         </form>
         <div className="text-center">
           <Link href="/auth/register" className="font-medium text-indigo-600 hover:text-indigo-500">
-            Don't have an account? Register
+            Don&apos;t have an account? Register
           </Link>
         </div>
       </div>
